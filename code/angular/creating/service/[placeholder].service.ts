67b8a0acd7740641c95aaf192103a51a37adca72
RitreshDigiscribeGithub <ritesh@digiscribe.in>
2023-03-22 17:15:16 +0530

    add service

--- code/angular/creating/service/[placeholder].service.ts
+++ code/angular/creating/service/[placeholder].service.ts
@@ -1,10 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
 export class PlaceholderService {
-   private heroes: any[] = [];
 
-   constructor() {}
-
-   getHeroes() {
-      this.heroes.push([1, 2, 3, 4]);
-      return this.heroes;
-   }
+  constructor() { }
 }
