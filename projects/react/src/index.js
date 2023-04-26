c377117fa540b196fef07bd90adeaf43733af6c2
RitreshDigiscribeGithub <ritesh@digiscribe.in>
2023-04-26 14:14:48 +0530

    add projects

---
+++ projects/react/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./assets/styles/style.scss";
+import { BrowserRouter } from "react-router-dom";
+import App from "./app/App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+   <BrowserRouter>
+      <App />
+   </BrowserRouter>
+);
+
+const link = document.createElement("link");
+link.setAttribute("href", "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css");
+link.setAttribute("rel", "stylesheet");
+link.setAttribute("crossorigin", "anonymous");
+document.getElementsByTagName("head")[0].appendChild(link);
