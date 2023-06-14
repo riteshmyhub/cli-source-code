ed7a8d488bcb5f52e05bbf36bd0de1337ed004f5
RitreshDigiscribeGithub <ritesh@digiscribe.in>
2023-06-14 19:39:34 +0530

    update ctx

--- code/react-typscript/creating/context-hook/use[Placeholder].ctx.tsx
+++ code/react-typscript/creating/context-hook/use[Placeholder].ctx.tsx
@@ -1,23 +1,16 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext } from "react";
 
 type CtxProps = {
    children: React.ReactNode;
 };
 type CtxType = {
-   user: UserType | null;
-   setUser: React.Dispatch<React.SetStateAction<UserType | null>>;
+   text: string | null;
 };
 
-type UserType = {
-   name: string;
-   profession: string;
-   age: number;
-};
 const PlaceholderContext = createContext<CtxType | null>(null);
 function PlaceholderProvider({ children }: CtxProps): JSX.Element {
-   const [user, setUser] = useState<UserType | null>(null);
    return (
-      <PlaceholderContext.Provider value={{ user, setUser }}>
+      <PlaceholderContext.Provider value={{ text: "sdsds" }}>
          {/* chlidren */}
          {children}
          {/* chlidren */}
@@ -31,28 +24,3 @@ function usePlaceholderCtx() {
 
 export { PlaceholderProvider, usePlaceholderCtx };
 
-/* 
-1 how to use in 
-   <PlaceholderProvider>
-     <ChildComponent/>
-   </PlaceholderProvider>
-
- 2  in ChildComponent.jsx  
-   const ChildComponent = () => {
-      const {user,setUser} = usePlaceholderCtx();
-      useEffect(() => {
-         user?.setUser({
-            name: "alex",
-            profession: "developer",
-            age: 28,
-         });
-       return () => {};
-      }, []);
-
-      return (
-         <div>
-            <p>{JSON.stringify(user)}</p>
-         </div>
-      );
-   };
-*/
